1694f4b913d868485c596f6ed49953c9f853ec26
Mina <minavu@Minas-MacBook-Pro.local>
2021-11-17 21:03:16 -0800

    Worked on navbar. Desktop view is good. Tablet view infrastructure is set.  Now just need the icons. Mobile view not done.  Can't get nav items to be on one line.

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,51 @@ import Trainers from './Trainers'
 import Welcome from './Welcome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Logo from './images/pokemon_logo.png';
 
 
 function App() {
   return (
         <BrowserRouter>
-            <Navbar className="flex-column justify-content-around side_menu" expand="lg" bg="dark" variant="dark"> 
+            {/* <Navbar className="flex-column justify-content-around side_menu" expand="lg" bg="dark" variant="dark">  */}
+            <Navbar className="menu_bar" expand="lg" bg="dark" variant="dark"> 
                 <Navbar.Brand className="m-auto">
-                    ICON HERE
+                    <img class="menu_logo_icon" />
+                    <img src={Logo} className="logo_img" alt="Pokemon Catch-Em-All Logo" />
                 </Navbar.Brand>
-                <Nav fill className="m-auto mt-0 w-100 flex-column align-items-start">
+                <Nav className="m-auto mt-0 w-100 flex-column align-items-start">
                     <Nav.Item>
-                        <Nav.Link href="/party">Party Planner</Nav.Link>
+                        <Nav.Link href="/party">
+                            <img class="menu_icon"  />
+                            <span className="menu_text">Party Planner</span>
+                        </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/pokedex">Pokedex</Nav.Link>
+                        <Nav.Link href="/pokedex">
+                            <img class="menu_icon" />
+                            <span className="menu_text">Pokedex</span>
+                        </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/trainers">Developers</Nav.Link>
+                        <Nav.Link href="/trainers">
+                            <img class="menu_icon"  />
+                            <span className="menu_text">Developers</span>
+                        </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/news">News</Nav.Link>
+                        <Nav.Link href="/news">
+                            <img class="menu_icon" />
+                            <span className="menu_text">News</span>
+                        </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/information">Information</Nav.Link>
+                        <Nav.Link href="/information">
+                            <img class="menu_icon" />
+                            <span className="menu_text">Information</span>
+                        </Nav.Link>
                     </Nav.Item>
                 </Nav>
+                <img class="menu_search_icon" />
                 <form className="m-auto">
                     <div>
                     <button className="btn btn-outline-secondary" type="submit">S</button>
