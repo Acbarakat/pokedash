99ee1b105d92caa3e187a2de7c30b53a1a8ebcbe
Mina <minavu@Minas-MacBook-Pro.local>
2021-11-17 19:22:28 -0800

    Update:  side menu, search bar

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,33 @@ import './App.css';
 function App() {
   return (
         <BrowserRouter>
-            <Navbar expand="lg" bg="dark" variant="dark" fixed="top"> 
-                <Container>
-                    <Navbar.Brand className="m-auto">ICON HERE</Navbar.Brand>
-                    <Nav fill className="m-auto w-100 ">
-                        <Nav.Item>
-                            <Nav.Link href="/pokedex">Pokedex</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/party">Party Planner</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/trainers">Trainers/Developers</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Container>
+            <Navbar className="flex-column justify-content-around side_menu" expand="lg" bg="dark" variant="dark"> 
+                <Navbar.Brand className="m-auto">
+                    ICON HERE
+                </Navbar.Brand>
+                <Nav fill className="m-auto mt-0 w-100 flex-column align-items-start">
+                    <Nav.Item>
+                        <Nav.Link href="/party">Party Planner</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/pokedex">Pokedex</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/trainers">Developers</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/news">News</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/information">Information</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <form className="m-auto">
+                    <div>
+                    <button className="btn btn-outline-secondary" type="submit">S</button>
+                    <input className="form-control search_input" type="search" placeholder="Search" />
+                    </div>
+                </form>
             </Navbar>
             <Switch>
                 <Route path="/pokedex">
