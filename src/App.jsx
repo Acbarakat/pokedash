3b8675ae508c10b1831d4b3c91d6ef0237524571
Allonte <angelosheltie@gmail.com>
2021-11-23 10:40:33 -0800

    Revert "Merge pull request #3 from Acbarakat/navbar"
    This reverts commit f13d79d722bedd65bee28d081e999a0f01797b38, reversing
    changes made to 3ff41fb42d96cd10f7f1387e7ebdafa84bef77ab.

--- src/App.jsx
+++ src/App.jsx
@@ -15,57 +15,26 @@ import Trainers from './Trainers'
 import Welcome from './Welcome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Logo from './images/pokemon_logo.png';
 
 
 function App() {
   return (
         <BrowserRouter>
-            {/* <Navbar className="flex-column justify-content-around side_menu" expand="lg" bg="dark" variant="dark">  */}
-            <Navbar className="menu_bar" expand="lg" bg="dark" variant="dark"> 
-                <Navbar.Brand className="m-auto">
-                    <img class="menu_logo_icon" />
-                    <img src={Logo} className="logo_img" alt="Pokemon Catch-Em-All Logo" />
-                </Navbar.Brand>
-                <Nav className="m-auto mt-0 w-100 flex-column align-items-start">
-                    <Nav.Item>
-                        <Nav.Link href="/party">
-                            <img class="menu_icon"  />
-                            <span className="menu_text">Party Planner</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/pokedex">
-                            <img class="menu_icon" />
-                            <span className="menu_text">Pokedex</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/trainers">
-                            <img class="menu_icon"  />
-                            <span className="menu_text">Developers</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/news">
-                            <img class="menu_icon" />
-                            <span className="menu_text">News</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/information">
-                            <img class="menu_icon" />
-                            <span className="menu_text">Information</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
-                <img class="menu_search_icon" />
-                <form className="m-auto">
-                    <div>
-                    <button className="btn btn-outline-secondary" type="submit">S</button>
-                    <input className="form-control search_input" type="search" placeholder="Search" />
-                    </div>
-                </form>
+            <Navbar expand="lg" bg="dark" variant="dark" fixed="top"> 
+                <Container>
+                    <Navbar.Brand className="m-auto">ICON HERE</Navbar.Brand>
+                    <Nav fill className="m-auto w-100 ">
+                        <Nav.Item>
+                            <Nav.Link href="/pokedex">Pokedex</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/party">Party Planner</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/trainers">Trainers/Developers</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Container>
             </Navbar>
             <Switch>
                 <Route path="/pokedex">
