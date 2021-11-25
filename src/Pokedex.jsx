7fe7be6e130a6452583337e3a8a25ad7cebd2d72
Allonte <angelosheltie@gmail.com>
2021-11-24 22:16:39 -0800

    Current In-Progress Pokedex page
    - Cannot resolve Certain ARIA roles must contain particular children due to the library

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { Card } from "react-bootstrap";
+import {Collection, AutoSizer} from 'react-virtualized';
 import { pokeapi } from "./common";
+import 'react-virtualized/styles.css'; // only needs to be imported once
 
 class PokedexEntry extends React.Component {
     componentDidMount() {
         pokeapi.get(`/pokemon/${this.props.name}`)
-        .then(async (response) => {
-            console.log(response.request.fromCache);
+        .then((response) => {
+            // console.log(response.request.fromCache);
             this.setState({...response.data});
         })
         .catch((error) => {
@@ -15,16 +16,20 @@ class PokedexEntry extends React.Component {
     }
 
     render(){
-        let sprite = this.state !== null ? this.state.sprites.front_default : "/pokeball.png";
+        let sprite = this.state !== null ? this.state.sprites.front_default : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+        let type = this.state !== null ? this.state.types : [{slot: 1, type: {name: "normal"}}];
+        let type1 = type[0].type.name;
+        let type2 = type.length === 2 ? type[1].type.name : type1;
 
         return (
-            <Card>
-                <Card.Img variant="top" src={sprite} />
-                {this.props.name}
-            </Card>
+            <div className={`entry type1-${type1} type2-${type2}`} style={this.props.style}>
+                <img src={sprite} alt={`${this.props.name} sprite`} />
+            </div>
         );
     }
 }
+
+const GUTTER = 20;
 
 class Pokedex extends React.Component {
     constructor(props) {
@@ -32,13 +37,43 @@ class Pokedex extends React.Component {
         
         this.state = {
             data: [],
+            columnCount: Math.floor(window.innerWidth / (100 + GUTTER)),
+            leftOffset: (window.innerWidth % 100) / 2
         };
+
+        this._cellRenderer = this._cellRenderer.bind(this);
+        this._cellSizeAndPositionGetter = this._cellSizeAndPositionGetter.bind(this);
+        this.updateDimensions = this.updateDimensions.bind(this);
+
+        this.collection = React.createRef();
+    }
+
+    updateDimensions() {
+        let {columnCount} = this.state;
+        let newCount = Math.floor(window.innerWidth / (100 + GUTTER));
+
+        this.setState({
+            columnCount: newCount,
+            leftOffset: (window.innerWidth % (100+GUTTER)) / 2
+        },() => {
+            console.log("Changed from", columnCount, "to", newCount);
+            this.collection.current.recomputeCellSizesAndPositions();
+        });
     }
 
     componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+
         pokeapi.get("/pokemon", {params: {limit: 9999}})
         .then(async (response) => {
             let { results } = response.data;
+            results.forEach((pokemon) => {
+                pokemon.id = parseInt(pokemon.url.split("/")[6]);
+                /*if (pokemon.id < 10000){
+                    return (<PokedexEntry key={pokemon.id} {...pokemon}/>);
+                }
+                return null;*/
+            });
             this.setState({data: results});
         })
         .catch((error) => {
@@ -46,19 +81,48 @@ class Pokedex extends React.Component {
         });
     }
 
+    _cellRenderer({index, key, style}){
+        let pokemon = this.state.data[index];
+
+        return (<PokedexEntry key={key} style={style} {...pokemon}/>);
+    }
+
+    _cellSizeAndPositionGetter({index}) {
+        let {columnCount, leftOffset} = this.state;
+        let y = Math.floor(index / columnCount);
+        y = (100 * y) + (GUTTER * (y + 1));
+        let x  = (index % columnCount);
+        x = (100 * x) + (GUTTER * x);
+        x += leftOffset - GUTTER > 0 ? leftOffset : GUTTER;
+
+        return {
+            height: 100,
+            width: 100,
+            x: x,
+            y: y,
+          };
+    }
+
     render(){
-        let pokemons = this.state.data.map((pokemon) => {
-            pokemon.id = parseInt(pokemon.url.split("/")[6]);
-            if (pokemon.id < 10000){
-                return (<PokedexEntry key={pokemon.id} {...pokemon}/>);
-            }
-            return null;
-        })
+        let navbarHeight = document.getElementsByClassName('navbar');
+        navbarHeight = navbarHeight.length !== 0 ? navbarHeight[0].offsetHeight : 0;
 
         return (
-            <div className="pokedex">
-                {pokemons}
-            </div>
+            <AutoSizer>
+            {({width, height}) => (
+                <main>
+                    <Collection
+                        ref={this.collection}
+                        cellCount={this.state.data.length}
+                        cellRenderer={this._cellRenderer}
+                        cellSizeAndPositionGetter={this._cellSizeAndPositionGetter}
+                        className="pokedex"
+                        height={height - navbarHeight}
+                        width={width}
+                    />
+                </main>
+            )}
+          </AutoSizer>
         )
     }
 }
