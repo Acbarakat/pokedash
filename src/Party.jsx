03b6e2684a1c785c66e5f8214c306e9bcfe510d7
Allonte <angelosheltie@gmail.com>
2021-11-24 17:18:38 -0800

    Remove unused pages

--- src/Party.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-class Party extends React.Component {
-    render(){
-        return "Lorem Ipsum"
-    }
-}
-
-export default Party;
--- src/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-class Welcome extends React.Component {
-    render(){
-        return "Lorem Ipsum"
-    }
-}
-
-export default Welcome;
